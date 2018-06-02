e152ec0224eab08497e7d07db0e7019fec0a20a9
mmulenga <matt.mulengawoo@gmail.com>
2018-06-01 19:24:06 -0600

    Converted to classes.

--- byaccidentbot.py
+++ byaccidentbot.py
@@ -2,57 +2,63 @@ import praw
 import re
 import threading
 
-def initCommentDict(dict):
-  try:
-    visitedCommentFile = open('visitedComments.txt', 'r')
-  except OSError:
-    print('File not found.')
-    return
+class ByAccidentBot:
+  def __init__(self):
+    self.commentDictionary = dict()
+    self.reddit = praw.Reddit('byaccidentbot', user_agent='pi:com.example.bybottest:v0.0.1 by /u/thecrazybandicoot')
+    self.searchPhrase = re.compile(r'.*\bon accident\b.*', flags=re.I)
+    self.account = self.reddit.user.me()
 
-  for keyValue in visitedCommentFile:
-    key,value = keyValue.split(' ')
-    dict[key] = value
+  def populateCommentDict(self, filename):
+    try:
+      visitedCommentFile = open(filename, 'r')
+    except OSError:
+      print('File not found.')
+      return
 
-# Searches for instances of 'on accident' within the given comment.
-# Pre:  phrase - A valid regex pattern.
-#       comment - A valid reddit comment.
-# Post: Returns true if a match is found, false otherwise.
-def searchForPhrase(phrase, comment):
-  if phrase.match(comment):
-    return True
-  else:
-    return False
+    for keyValue in visitedCommentFile:
+      key,value = keyValue.split(' ')
+      self.commentDictionary[key] = value
+    
+    visitedCommentFile.close()
 
-# Replies to the given comment with a pre-written message.
-# Pre: comment - A valid reddit comment.
-# Post: Replies to the comment with the appropriate reply template.
-def replyToComment(comment):
-  comment.reply('Just a friendly reminder that it\'s \"by accident\".')
+  # Searches for instances of 'on accident' within the given comment.
+  # Pre:  phrase - A valid regex pattern.
+  #       comment - A valid reddit comment.
+  # Post: Returns true if a match is found, false otherwise.
+  def searchForPhrase(self, comment):
+    if self.searchPhrase.match(comment):
+      return True
+    else:
+      return False
 
-# Automatically deletes any comment that gets downvoted below 0, checking
-# every hour to see if the score has changed.
-# Gives users a method to delete the comment if they dislike it.
-def autoDeleteScoreCheck(user):
-  threading.Timer(3600.0, autoDeleteScoreCheck, [user]).start()
+  # Replies to the given comment with a pre-written message.
+  # Pre: comment - A valid reddit comment.
+  # Post: Replies to the comment with the appropriate reply template.
+  def replyToComment(self, comment):
+    comment.reply('Just a friendly reminder that it\'s \"by accident\".')
 
-  for comment in user.comments.new(limit=50):
-    if comment.score < 0:
-      comment.delete()
+  # Automatically deletes any comment that gets downvoted below 0, checking
+  # every hour to see if the score has changed.
+  # Gives users a method to delete the comment if they dislike it.
+  def autoDeleteScoreCheck(self):
+    threading.Timer(3600.0, autoDeleteScoreCheck, [self.account]).start()
 
-def main():
-  commentDictionary = dict()
-  reddit = praw.Reddit('byaccidentbot', user_agent='pi:com.example.bybottest:v0.0.1 by /u/thecrazybandicoot')
-  phrase = re.compile(r'.*\bon accident\b.*', flags=re.I)
-  commentStream = reddit.subreddit('byaccidentbot').stream.comments()
-  botAccount = reddit.user.me()
+    for comment in self.account.comments.new(limit=50):
+      if comment.score < 0:
+        comment.delete()
 
-  # Start score checker.
-  autoDeleteScoreCheck(botAccount)
+  def main():
+    bot = ByAccidentBot()
+    commentStream = reddit.subreddit('byaccidentbot').stream.comments()
 
-  for comment in commentStream:
-    if searchForPhrase(phrase, comment.body):
-      replyToComment(comment)
-      print('ID: ' + comment.id + ' ' + comment.body)
+    # Start score checker.
+    bot.autoDeleteScoreCheck()
 
-if __name__ == "__main__":
+    for comment in commentStream:
+      if searchForPhrase(phrase, comment.body):
+        replyToComment(comment)
+        print('ID: ' + comment.id + ' ' + comment.body)
+
+  if __name__ == "__main__":
     main()
--- testing/testbyaccidentbot.py
+++ testing/testbyaccidentbot.py
@@ -1,16 +1,25 @@
 import unittest
-import byaccidentbot
+import praw
+import re
+from byaccidentbot import ByAccidentBot
 
-class testbyaccidentbot(unittest.TestCase):
+class TestByAccidentBot(unittest.TestCase):
+  def setUp(self):
+    self.bot = ByAccidentBot()
+
   # Tests the regular expression to see what types of matches it comes up with
-  # can be commented out for prod.
   # Pre: file - A valid text file containing test phrases.
   # Post: Output of all phrases matching the regex pattern.
-  def test_searchForPhrase(self):
-    self.assertTrue()
+  ##  self.assertTrue()
 
   def test_initCommentDict(self):
-    self.assertEqual()
+    with self.assertRaises(OSError):
+      self.bot.populateCommentDict('fakefile.txt')
+
+    self.assertEqual(len(self.bot.commentDictionary), 0)
+    self.bot.populateCommentDict('testing/visitedComments.txt')
+    self.assertEqual(len(self.bot.commentDictionary), 1)
+    self.assertIn('test', self.bot.commentDictionary)
   
   if __name__ == "__main__":
     unittest.main()
